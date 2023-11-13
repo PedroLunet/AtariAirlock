d7b0214a34aa770bf0480fb3afad0055369752f0
Pedro Lunet <pedrolunet@gmail.com>
2023-11-13 18:21:57 +0000

    Updated App class and created Game class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,7 @@
+import java.io.IOException;
+
 public class App {
+    public static void main(String[] args) throws IOException {
+        new Game(80, 24).run();
+    }
 }
---
+++ src/main/java/Game.java
@@ -0,0 +1,26 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Game {
+    private final TerminalScreen screen;
+    public Game(int  width, int height) throws IOException {
+        Terminal terminal = new DefaultTerminalFactory().setInitialTerminalSize(new TerminalSize(width, height)).createTerminal();
+        screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null);   // we don’t need a cursor
+        screen.startScreen();             // screens must be started
+        screen.doResizeIfNecessary();     // resize screen if necessary
+        TerminalSize terminalSize = new TerminalSize(width, height);
+    }
+    private void draw() throws IOException {
+        screen.clear();
+
+        screen.refresh();
+    }
+    public void run() throws IOException {
+        draw();
+    }
+}
