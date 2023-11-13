5ee6d8c002c0fe435fef4bc7cbafe2b47df8582c
antonioccunha <antonio.cacao.cunha00@gmail.com>
2023-11-13 18:18:31 +0000

    Created Position class

---
+++ src/main/java/Position.java
@@ -0,0 +1,19 @@
+public class Position {
+    private int x ;
+    private int y ;
+
+    //Default Constructor
+    public Position(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    //Getters & Setters
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
+    //Methods
+    public boolean samePosition(Position p1, Position p2){
+        return p1.getX() == p2.getX() && p1.getY() == p2.getY();
+    }
+}
