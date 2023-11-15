a7d5f2129359a2a2055aeec4207e9333d4e78cdc
antonioccunha <antonio.cacao.cunha00@gmail.com>
2023-11-15 20:50:07 +0000

    Added extra function to Position class

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -1,19 +1,38 @@
 public class Position {
-    private int x ;
-    private int y ;
+    private int x;
+    private int y;
 
     //Default Constructor
-    public Position(int x, int y){
+    public Position(int x, int y) {
         this.x = x;
         this.y = y;
     }
+
     //Getters & Setters
-    public int getX() { return x; }
-    public int getY() { return y; }
-    public void setX(int x) { this.x = x; }
-    public void setY(int y) { this.y = y; }
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     //Methods
-    public boolean samePosition(Position p1, Position p2){
+    public boolean samePosition(Position p1, Position p2) {
         return p1.getX() == p2.getX() && p1.getY() == p2.getY();
     }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
 }
