b151c60e9619647b1ad4c81c2de5ae68c02c9d92
antonioccunha <antonio.cacao.cunha00@gmail.com>
2023-11-15 20:51:20 +0000

    Created Monster class

---
+++ src/main/java/Monster.java
@@ -0,0 +1,19 @@
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+public class Monster extends Element {
+
+    public Monster(int x,int y){
+        super(x,y);
+    }
+
+    @Override
+    public void draw(TextGraphics graphics) {
+        graphics.setForegroundColor(TextColor.Factory.fromString("#000000"));//black for now
+        graphics.putString(new TerminalPosition(position.getX(), position.getY()), "M");
+    }
+
+    //Future Ideas
+    //Maybe add monster types?
+}
